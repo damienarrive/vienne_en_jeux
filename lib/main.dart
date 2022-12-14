c3e77e72abc750701cce5680d858e4d54826c818
MaelleGARNIER <maellegn99@gmail.com>
2022-12-14 14:48:26 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bonsoir à tous les amis',
+      title: 'Bonsoir à tous les amis!!!',
       theme: ThemeData(
         // This is the theme of your application.
         //
