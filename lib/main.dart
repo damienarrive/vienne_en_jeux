0a491ed9c3c3cb0fbf44038a90c78c37468be27c
CBerthometPeyrat <cy.berthomet-peyrat@laposte.net>
2022-12-14 14:44:19 +0100

    bonjour

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bonsoir à tous les amis',
       theme: ThemeData(
         // This is the theme of your application.
         //
