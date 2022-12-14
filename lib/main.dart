e874ca1522918387a110e9a241d0900dc9d718cd
DamienArriveAmexio <darrive@amexio.fr>
2022-12-14 15:01:39 +0100

    test brancheeeee

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ekip ekip',
+      title: 'Eje test le zizi caca',
       theme: ThemeData(
         // This is the theme of your application.
         //
